69d87febd15c1b1cade455e8f0de1616adfc09ce
Hasarian <romain.demoustiez@gmail.com>
2018-03-16 18:56:41 +0100

    -arrangement du formulaire
    il reste tout à faire

--- labo5/SubscribeFormPanel.java
+++ labo5/SubscribeFormPanel.java
@@ -5,19 +5,44 @@ import java.awt.*;
 
 public class SubscribeFormPanel extends JPanel
 {
-    JLabel nameLabel,firstNameLabel;
-    JTextField name,firstName;
+    private JLabel nameLabel,firstNameLabel;
+    private JTextField name,firstName;
     public SubscribeFormPanel()
     {
         super();
-        setLayout(new GridLayout(2,2));
+        setLayout(new GridBagLayout());
         nameLabel=new JLabel("family name:");
         firstNameLabel=new JLabel("first name:");
         name=new JTextField();
         firstName=new JTextField();
-        add(nameLabel);
-        add(firstNameLabel);
-        add(name);
-        add(firstName);
+
+        GridBagConstraints contraints= new GridBagConstraints();
+        contraints.fill=GridBagConstraints.HORIZONTAL;
+        contraints.anchor=GridBagConstraints.EAST;
+        contraints.weighty=1;
+        contraints.gridwidth=3;
+        contraints.gridheight=1;
+        contraints.gridx=5;
+        contraints.gridy=5;
+        contraints.weightx=1;
+        add(nameLabel,contraints);
+        contraints.anchor=GridBagConstraints.WEST;
+        contraints.gridx+=contraints.gridwidth+1;
+        contraints.gridwidth++;
+        contraints.weightx=2;
+        add(name,contraints);
+        contraints.anchor=GridBagConstraints.EAST;
+        contraints.weightx--;
+        contraints.gridy+=contraints.gridy;
+        contraints.gridx=5;
+        contraints.gridwidth--;
+        add(firstNameLabel,contraints);
+        contraints.anchor=GridBagConstraints.WEST;
+        contraints.gridx+=contraints.gridwidth+1;
+        contraints.gridwidth++;
+        contraints.weightx++;
+        add(firstName,contraints);
+
+
     }
 }
